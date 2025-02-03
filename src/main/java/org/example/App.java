7394b7bf3a6482fae6ee6feddb9c4d2a194ddf49
Alen Mujkanović <mujkalen@gmail.com>
2025-02-03 21:04:36 +0100

    1.zadatak

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class App {
 
@@ -10,4 +14,35 @@ public class App {
     public static void main(String[] args) {
 
     }
+
+    public static void getAuthors() {
+        Session session = sessionFactory.openSession();
+
+        List<Author> authors = session.createQuery("FROM Author a JOIN FETCH a.books", Author.class).list();
+
+        for (Author author : authors) {
+            System.out.println("Author: " + author.getName());
+            for (Book book : author.getBooks()) {
+                System.out.println("  Book: " + book.getTitle());
+            }
+        }
+
+        session.close();
+    }
+
+    public static void updateBookTitle(Long id, String newTitle){
+        Session session  = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("UPDATE Book b SET b.title=:title WHERE b.id=:id")
+                .setParameter("title", newTitle)
+                .setParameter("id", id);
+        transaction.commit();
+        session.close();
+        System.out.println("Book title updated.");
+    }
+
+
+
+
 }
+
